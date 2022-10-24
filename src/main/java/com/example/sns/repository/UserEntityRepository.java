452709aea9de4c1d6c4b9471f8c3c877a31f13db
metacogpe <metacog@kakao.com>
2022-10-24 12:04:39 +0900

    UserEntityRepository인터페이스 내에 findByUsername(String userName)함수 정의

--- src/main/java/com/example/sns/repository/UserEntityRepository.java
+++ src/main/java/com/example/sns/repository/UserEntityRepository.java
@@ -4,6 +4,9 @@ import com.example.sns.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String userName);  // 검색 결과는 옵션널 : 결과 없을 수 있음
 }
