f044b4363f512d6b253619cc98aa3e53e2d8e277
metacogpe <metacog@kakao.com>
2022-10-24 14:37:44 +0900

    정의한 findByUsername()함수를 UserService에서 사용

--- src/main/java/com/example/sns/service/UserService.java
+++ src/main/java/com/example/sns/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.sns.service;
 
 import com.example.sns.model.User;
+import com.example.sns.model.entity.UserEntity;
 import com.example.sns.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +18,7 @@ public class UserService {
     public User join(String userName, String password) {
         // 회원 가입에는 2가지가 필요 : 체크 후에 등록
         // 1) 체크 : 회원가입하려는 userName 으로 회원가입된 user 가 있는지
-        userEntityRepository.findByUsername();
+        Optional<UserEntity> userEntity = userEntityRepository.findByUsername(userName);  //정의한 findByUsername()함수 사용
         // 2) 등록 : 회원가입 진행 = user 를 등록
         //userEntityRepository.save();
         return new User();
