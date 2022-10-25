ac51d7829fe5d613aacd77beeeb955c6a58fa540
metacogpe <metacog@kakao.com>
2022-10-25 17:07:25 +0900

    UserController  @PostMapping의 join메소드 body 내용 적용

--- src/main/java/com/example/sns/controller/UserController.java
+++ src/main/java/com/example/sns/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.sns.controller;
 
+import com.example.sns.controller.request.UserJoinRequest;
 import com.example.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,10 @@ public class UserController {
 
     private final UserService userService; // UserService 불러오기
     // todo : implement
-    @PostMapping
-    public void join() {
+    @PostMapping("/join")
+    // UserJoinRequest 클래스에 정의된 userName 과 password 를 RequestBody 로 불러옴
+    public void join(@RequestBody UserJoinRequest request) {
         // join
-        userService.join("", "");   // 불러온 userService의 join() 사용 가능
+        userService.join(request.getUserName(), request.getPassword()); // UserJoinRequest 로 불러온 userName 과 password 를 userService 로 넘김
     }
 }
