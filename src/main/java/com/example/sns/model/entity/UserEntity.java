e61909c3af6b9c90cfd5c97aa091313d79d6ce2e
metacogpe <metacog@kakao.com>
2022-10-25 17:24:36 +0900

    엔터티에 컬럼 추가 : 생성/업데이트/삭제 시간 (데이터 관리 용이성)

--- src/main/java/com/example/sns/model/entity/UserEntity.java
+++ src/main/java/com/example/sns/model/entity/UserEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -19,5 +20,16 @@ public class UserEntity {
 
     @Column(name = "user_name")
     private String userName;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;  // 생성시간 : 데이터 관리 용이성 위한 용도
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;     // 업데이트시간
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;    // 삭제시간
 }
