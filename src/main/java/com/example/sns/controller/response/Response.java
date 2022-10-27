90f7480ec9b92c55cb7c5e3688a5935d9b2da456
metacogpe <metacog@kakao.com>
2022-10-28 08:58:16 +0900

    Response 클래스를 제네릭 타입으로 선언

--- src/main/java/com/example/sns/controller/response/Response.java
+++ src/main/java/com/example/sns/controller/response/Response.java
@@ -1,4 +1,7 @@
 package com.example.sns.controller.response;
 
-public class Response {
+public class Response<T> {
+
+    private String resultcode;
+    private T result; // 결과값 타입이 다를 수 있어 제네릭으로 선언 : UserJoinResponse 타입, LoginResponse 타입 등 다양한 결과값 타입 존재
 }
