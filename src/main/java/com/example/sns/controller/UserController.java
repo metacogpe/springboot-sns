6b6f8a6ec2869562089aea226b0c644f8c3ca10c
metacogpe <metacog@kakao.com>
2022-10-28 08:31:33 +0900

    회원가입인 join에 대해 User클래스로 받아 줌

--- src/main/java/com/example/sns/controller/UserController.java
+++ src/main/java/com/example/sns/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.sns.controller;
 
 import com.example.sns.controller.request.UserJoinRequest;
+import com.example.sns.model.User;
 import com.example.sns.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,6 @@ public class UserController {
     // UserJoinRequest 클래스에 정의된 userName 과 password 를 RequestBody 로 불러옴
     public void join(@RequestBody UserJoinRequest request) {
         // join
-        userService.join(request.getUserName(), request.getPassword()); // UserJoinRequest 로 불러온 userName 과 password 를 userService 로 넘김
+        User user = userService.join(request.getUserName(), request.getPassword()); // UserJoinRequest 로 불러온 userName 과 password 를 userService 로 넘김
     }
 }
