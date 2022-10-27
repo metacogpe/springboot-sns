8913b5d68b9e97222cb76a9ac76f1a6cb1923f07
metacogpe <metacog@kakao.com>
2022-10-28 07:14:25 +0900

    PrePersist/PreUpdate 매번 입력의 불편함 해소 : 저장 되기 전에 현재 시각 저장

--- src/main/java/com/example/sns/model/entity/UserEntity.java
+++ src/main/java/com/example/sns/model/entity/UserEntity.java
@@ -2,17 +2,19 @@ package com.example.sns.model.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.time.Instant;
 
 @Getter
 @Setter
 @Entity
-@Table
+@Table(name = "\"user\"")
+@SQLDelete(sql = "UPDATED \"user\" SET deleted_at = NOW() where id=?")  // delete sql 시 삭제시간 업데이트
+@Where(clause = "deleted_at is NULL")  // 삭제가 안된 것만 가져오도록 구현
 public class UserEntity {
 
     @Id   // 자동 increase
@@ -32,4 +34,14 @@ public class UserEntity {
 
     @Column(name = "deleted_at")
     private Timestamp deletedAt;    // 삭제시간
+
+    @PrePersist  // registerdAt 매번 입력의 불편함 해소 : 저장 되기 전에 현재 시각 저장
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate  // 업데이트전에도 업데이트 시각 저장
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
 }
