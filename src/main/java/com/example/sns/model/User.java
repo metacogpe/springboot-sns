238917f0f805f9752495b1bb4a950c2052d8585b
metacogpe <metacog@kakao.com>
2022-10-28 08:19:51 +0900

    엔터티를 DTO로 변환해 주는 메소드 생성

--- src/main/java/com/example/sns/model/User.java
+++ src/main/java/com/example/sns/model/User.java
@@ -1,8 +1,32 @@
 package com.example.sns.model;
 
-// todo
+import com.example.sns.model.entity.UserEntity;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.sql.Timestamp;
+
+@AllArgsConstructor
 public class User {
 
+    private Integer id;
     private String userName;
     private String password;
+    private UserRole userRole;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    // 엔터티를 DTO 로 변환
+    public static User fromEntity(UserEntity entity) {
+        return new User(
+                entity.getId(),
+                entity.getUserName(),
+                entity.getPassword(),
+                entity.getRole(),
+                entity.getRegisteredAt(),
+                entity.getUpdatedAt(),
+                entity.getDeletedAt()
+        );
+    }
 }
