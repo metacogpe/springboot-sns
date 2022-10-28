49a33510baaab36f67e82b673576655107c5a861
metacogpe <metacog@kakao.com>
2022-10-28 14:20:38 +0900

    에러코드 하나 정의 : HttpStatus, message

--- src/main/java/com/example/sns/exception/ErrorCode.java
+++ src/main/java/com/example/sns/exception/ErrorCode.java
@@ -1,4 +1,17 @@
 package com.example.sns.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorCode {
+
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "Duplicate username" ),
+    ;
+
+    // 에러코드 마다 http status 정의 위한 용도
+    private HttpStatus status;
+    private String message;
 }
