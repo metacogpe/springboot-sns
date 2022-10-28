b2202cb671bbf6ccd9e0dc6cafa852e0a2aafe79
metacogpe <metacog@kakao.com>
2022-10-28 14:55:39 +0900

    예외처리 클래스에서 에러코드와 메시지 리턴하도록 getMessage() 오버라이딩

--- src/main/java/com/example/sns/exception/SnsApplicationException.java
+++ src/main/java/com/example/sns/exception/SnsApplicationException.java
@@ -1,7 +1,19 @@
 package com.example.sns.exception;
 
+import lombok.AllArgsConstructor;
+
 // todo : implement
+@AllArgsConstructor
 public class SnsApplicationException extends RuntimeException {
 
     private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String getMessage() {
+        if (message == null) {
+            return errorCode.getMessage();
+        }
+        return String.format("%s. %s", errorCode, getMessage(), message);
+    }
 }
